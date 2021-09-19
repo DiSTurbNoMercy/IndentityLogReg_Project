7d018339dd3aecb8ec65f61fc3aed5bc8b491227
Vladyslav Kovalenko <kowaly2016@gmail.com>
2021-09-19 02:17:41 +0200

    Add project files.

---
+++ IndentityLogReg_Project/ViewMdl/Register.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IndentityLogReg_Project.ViewMdl
+{
+    public class Register
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string EMail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage ="Password and Confornation password DO NOT MATCH!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
